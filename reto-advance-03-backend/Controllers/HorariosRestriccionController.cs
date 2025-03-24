cb73a81cd1044d215544eee5d0c40db42d80d0bd
Stalin Alejandro Amagua Sandobalín <jueltter@users.noreply.github.com>
2025-03-24 05:28:27 -0500

    add /horarios endpoint

---
+++ reto-advance-03-backend/Controllers/HorariosRestriccionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using reto_advance_03_backend.Entities;
+using reto_advance_03_backend.Repositories;
+
+namespace reto_advance_03_backend.Controllers
+{
+    [Route("horarios")]
+    [ApiController]
+    public class HorariosRestriccionController : ControllerBase
+    {
+        private readonly IHorarioRestriccionRepository _repo;
+        private readonly ILogger<HorariosRestriccionController> _logger;
+
+        public HorariosRestriccionController(IHorarioRestriccionRepository repo, ILogger<HorariosRestriccionController> logger)
+        {
+            _repo = repo;
+            _logger = logger;
+        }
+
+        [HttpGet("")]
+        public ActionResult<List<HorarioRestriccion>> FindAll()
+        {
+            return Ok(_repo.FindAll());
+        }
+    }
+}
